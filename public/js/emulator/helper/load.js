ee89f9ef8b3a77c349444034e6759e6c1e2dc3a3
Andrew Thiele <308butterfly@gmail.com>
2023-12-26 13:05:44 -0600

    roms load added prettier config

--- public/js/emulator/helper/load.js
+++ public/js/emulator/helper/load.js
@@ -1,32 +1,25 @@
-export const ROM_LOCATIONS = [
-    './public/assets/roms/space_invader_roms/invaders.h',
-    './public/assets/roms/space_invader_roms/invaders.g',
-    './public/assets/roms/space_invader_roms/invaders.f',
-    './public/assets/roms/space_invader_roms/invaders.e'
-  ];
-  
-  export async function fetchROM(location) {
-    try {
-      const response = await fetch(location);
-      if (!response.ok) {
-        throw new Error(`Failed to fetch ROM from ${location}`);
-      }
-      return new Uint8Array(await response.arrayBuffer());
-    } catch (error) {
-      console.error(`Fetching error for ROM at ${location}:`, error);
-      throw error; // Re-throw to handle in the calling function
+export const ROM_LOCATIONS = ['./public/assets/roms/invaders.h', './public/assets/roms/invaders.g', './public/assets/roms/invaders.f', './public/assets/roms/invaders.e'];
+
+export async function fetchROM(location) {
+  try {
+    const response = await fetch(location);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ROM from ${location}`);
     }
+    return new Uint8Array(await response.arrayBuffer());
+  } catch (error) {
+    console.error(`Fetching error for ROM at ${location}:`, error);
+    throw error; // Re-throw to handle in the calling function
   }
-  
-  export async function flashROMBank(romLocation ) {
-    try {
-      const romPartions = await Promise.all(romLocations.map(fetchROM));
-  
-      return romPartions;
-        
-    } catch (error) {
-      console.error('Error in flashing ROM bank:', error);
-      return null; // Or handle error differently
-    }
+}
+
+export async function flashROMBank(romLocations) {
+  try {
+    const romPartions = await Promise.all(romLocations.map(fetchROM));
+
+    return romPartions;
+  } catch (error) {
+    console.error('Error in flashing ROM bank:', error);
+    return null; // Or handle error differently
   }
-  
+}
--- public/js/emulator/main.js
+++ public/js/emulator/main.js
@@ -1,8 +1,15 @@
-
+import { Z8080_BUS } from './8080.js';
 
 function start() {
-    
+  var canvas = document.getElementById('vertical_screen');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#FFC0CB'; // Pink color
+    ctx.fillRect(50, 50, 100, 100); // Draw a 100x100 square starting at (50, 50)
+  }
+  Z8080_BUS.load();
 }
 
-
-
+document.addEventListener('DOMContentLoaded', event => {
+  document.getElementById('start').addEventListener('click', start);
+});
